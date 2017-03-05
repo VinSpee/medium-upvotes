3e38694af52fe52b6b9eecc60dc3c062ca739596
Vince Speelman <vince@ted.com>
2017-03-05 11:17:21 -0500

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Medium Upvotes
+
+[Medium](http://medium.com) has a nice [API], but it doesn't give us what we
+all _really_ want, __DAT UPVOTE COUNT BOI__.
+
+This is a simple microservice built on [zeit's
+micro](https://github.com/zeit/microhttps://github.com/zeit/micro),
+[got](https://github.com/sindresorhus/gothttps://github.com/sindresorhus/got),
+and
+[cheerio](https://github.com/cheeriojs/cheeriohttps://github.com/cheeriojs/cheerio)
+that scrapes that upvote number and returns it to you.
+
+## USAGE
+
+all you need is a `post` property, which can be:
+
+- a Medium post URL: `https://medium.com/swlh/the-nine-states-of-design-5bfe9b3d6d85#.we8yp3ykt`
+- a Medium vanity URL:
+	`https://magenta.as/the-secret-to-great-design-a-healthy-tolerance-for-failure-f8d22b23bbd9#.jaq7e7q4i`
+- a Medium post ID: `5bfe9b3d6d85`
+
+Just post to your endpoint (the URL here is a demo endpoint running this repo's
+code):
+
+```sh
+curl -H "Content-Type: application/json" -X POST -d '{"post": "https://medium.com/swlh/the-nine-states-of-design-5bfe9b3d6d85#.we8yp3ykt"}' https://my-gomix-app-mjzvrsskrg.now.sh
+```
+
+and get back:
+
+```sh
+{"data":{"upvotes":"2100","id":"5bfe9b3d6d85"}}
+```
+
+
+That's it!
+
+### REQUIREMENTS
+- node v6 or later
---
+++ index.js
@@ -0,0 +1,77 @@
+const cheerio = require('cheerio');
+const got = require('got');
+const {
+  send,
+  json,
+  createError,
+  sendError,
+} = require('micro');
+
+const formatNumber = (num) => {
+  if (!num) {
+    throw createError(404, 'No upvote data found in the target page');
+  }
+  if (/K$/.exec(num)) {
+    const formattedNum = /(\d)\.?(\d)?K/.exec(num);
+    return `${formattedNum[1] ? formattedNum[1] : ''}${formattedNum[2] ? `${formattedNum[2]}00` : '000'}`;
+  }
+  return num;
+};
+
+const getVoteCount = (html) => {
+  if (!html) {
+    throw createError(404, 'No upvote data found in the target page');
+  }
+  const $ = cheerio.load(html);
+  let postID;
+  $('script').each((i, el) => {
+    const matches = $(el).text().match(/{"id":"(\w+)",/);
+    postID = matches && matches[1];
+  });
+  
+  return {
+    upvotes: formatNumber($(`[data-post-id="${postID}"] [data-action="show-recommends"]`)
+      .map((i, el) => $(el).text()).get()[0]),
+    id: postID,
+  };
+};
+
+const validateURL = (url) => {
+  const urlRegex = /(^http)s?:\/\/(?:.+\.\w+)\/(?:.+\/)?(.+)(?:[#|\?]?.+)$|(?:^(?!https?:\/\/))(\w+)/;
+  const match = url.match(urlRegex);
+  if (match && match.length >= 4 && match[3]) {
+    return `https://medium.com/posts/${url}`;
+  }
+  if (match && match.length >= 3 && match[2]) {
+    return url;
+  }
+  if (match && match.length >= 2 && match[1]) {
+    return url;
+  }
+  throw createError(400, 'Not a valid medium post');
+}
+
+module.exports = async (req, res) => { 
+  let postURL;
+  try {
+    const { post } = await json(req);
+    postURL = post;
+  } catch(err) {
+    sendError(req, res, err);
+  }
+  const isValid = validateURL(postURL);
+  let html;
+  try {
+    html = await got(isValid);
+  } catch(err) {
+    return sendError(req, res, err);
+  }
+  try {
+    const number = getVoteCount(html.body);
+    return send(res, 200, JSON.stringify({ data: number, }));
+  } catch (err) {
+    if (err === 'No upvote data found in the target page') {
+      return sendError(req, res, err);
+    }
+  }
+};
